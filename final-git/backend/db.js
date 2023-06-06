678cc1fce592d9c090190a8637563bbd318bfe8d
Mayank Jain <77920812+mayank3657@users.noreply.github.com>
2023-06-06 09:15:46 +0530

    Add files via upload

---
+++ final-git/backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const url ="mongodb://0.0.0.0:27017/viva";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
